c7bede7c2f92a39e3822a7192f16438a2b64e05e
Haripriya Chittari <hpvarma09@gmail.com>
2022-11-23 15:46:16 +0530

    added author

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const quote = document.getElementById('quote');
 const quoteId = document.getElementById('quoteId');
 // const quoteBtn=document.getElementById('quote-btn');
 const dark = document.getElementById('dark-theme');
+const author1=document.getElementById('author');
 
 async function fetchQuote() {
 
     const res = await fetch("https://type.fit/api/quotes");
     const results = await res.json();
     let randomId = Math.floor(Math.random() * results.length)
-    console.log(results[randomId].text);
-    const { text } = results[randomId];
+    console.log(results[randomId].text,results[randomId].author);
+    const { text, author } = results[randomId];
     quoteId.innerText = "#" + randomId;
     quote.innerText = text;
+    author1.innerText= "~ "+author;
 }
 // fetchQuote();
 
